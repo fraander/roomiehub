c99e9bdb799a5ebc96eff3c58d14e88b2e34fa2e
fraander <57777918+fraander@users.noreply.github.com>
2022-02-21 18:59:23 -0500

    started design of a new chore modal

--- roomie-hub/src/App.js
+++ roomie-hub/src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+
+const NewChoreModal = () => {
+  return (
+    <div style={{padding: 20}}>
+      <fieldset>
+        <legend><h2>Create new chore</h2></legend>
+
+        <p>
+          <span>Title: </span><input type="text"></input>
+        </p>
+
+        <p>
+          <span>Due Date: </span><input type="date"></input>
+        </p>
+
+        <p>
+          <span>Assignee: </span>
+          <select>
+            <option value="frank">Frank</option>
+            <option value="veena">Veena</option>
+            <option value="theo">Theo</option>
+          </select>
+        </p>
+
+        <p>
+          <p>Description: </p>
+          <textarea name="description"></textarea>
+        </p>
+
+      </fieldset>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Roomie Hub</h1>
+        <p>Keep track of chores with your roommates!</p>
+
+
+        <NewChoreModal />
       </header>
     </div>
   );
