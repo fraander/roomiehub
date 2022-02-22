174edb08bc778d9d2fec4b15e2bae30e5d5d51aa
fraander <57777918+fraander@users.noreply.github.com>
2022-02-22 09:41:58 -0500

    Update App.js

--- roomie-hub/src/App.js
+++ roomie-hub/src/App.js
@@ -3,56 +3,59 @@ import './App.css';
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Stack from '@mui/material/Stack';
 
-const preview_model = {
-  users: {
-    user_f: "Frank",
-    user_t: "Theo",
-    user_v: "Venna"
-  },
-  chores: {
-    chore_1: {
-      title: "Take out trash",
-      // assignee: user_f,
-      description: "Bag is full. Once trash is taken out the bag also needs replacing."
-    }
-  }
-}
+const preview_roommates = ["Frank", "Theo", "Venna"]
+
+// const preview_chores = {
+//   chore_1: {
+//     title: "Take out trash",
+//     // assignee: user_f,
+//     description: "Bag is full. Once trash is taken out the bag also needs replacing."
+//   }
+// }
 
 const NewChoreModal = () => {
-  const roommates = ["Frank", "Veena", "Theo"] // get this list from the backend
+  const roommates = preview_roommates // get this list from the backend
 
   return (
-    <div style={{ padding: 20 }}>
-      <fieldset>
-        <legend><h2>Create new chore</h2></legend>
+    <div>
+      <fieldset> {/*Need to style the fieldset a little more to match*/}
+        <Stack spacing={2}>
+          <legend><h2>Create new chore</h2></legend>
 
-        <TextField id="filled-basic" label="Chore Title" variant="outlined" />
+          <TextField id="filled-basic" label="Chore Title" variant="outlined" />
 
-        <p> {/* Convert to MUI version */}
-          <span>Due Date: </span><input type="date"></input>
-        </p>
-        
-        <p> {/* Convert to MUI version */}
-          <span>Assignee: </span>
-          <select>
+          <TextField
+            id="date"
+            label="Due Date"
+            type="date"
+            defaultValue="2017-05-24" // set to today by default
+          />
+
+          <Select
+            label="Assignee" // Make this show up
+            value="Frank" // Make this update properly
+          >
             {roommates.map((roommate) => (
-              <option value={roommate}>{roommate}</option>
+              <MenuItem value={roommate}>{roommate}</MenuItem>
             ))}
-          </select>
-        </p>
+          </Select>
 
-        <TextField // need to make width flexible
-          id="outlined-multiline-static"
-          label="Description"
-          multiline
-          rows={4}
-          defaultValue=""
-        />
-        <p>
-          <Button variant="contained">Create Chore</Button>
-        </p>
+          <TextField // need to make width flexible
+            id="outlined-multiline-static"
+            label="Description"
+            multiline
+            rows={4}
+            defaultValue=""
+          />
+          <p>
+            <Button variant="contained">Create Chore</Button>
+          </p>
 
+        </Stack>
       </fieldset>
     </div>
   );
